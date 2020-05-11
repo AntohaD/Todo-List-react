e1ee1faecc9088c6bb9d6e58f90d53c8a1b01506
Anton Dzherdzh <antondzherdzh@gmail.com>
2020-05-12 00:19:49 +0300

    init commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const About = () => (
+  <div className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">React app</h1>
+      <p className="lead">
+        Version of App <strong>1.0.1</strong>
+      </p>
+    </div>
+  </div>
+)
